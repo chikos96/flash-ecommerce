f87a381431efd3f792501a9f4fd9f25cc23e2faf
chikos96 <tchikos741@gmail.com>
2021-04-30 11:06:56 +0000

    deploying to heroku part 2

--- flash/settings.py
+++ flash/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-uek9_@c35c9ahasur3f7h3v3^w0pwmrvr&^6zt=9&qp6*qu1))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chikos-flash-ecommerce.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,12 +122,17 @@ WSGI_APPLICATION = 'flash.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
