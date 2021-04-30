20bf66a83738c882306828b1ede52aa7c1a2d496
chikos96 <tchikos741@gmail.com>
2021-04-30 11:22:30 +0000

    removed secret key and debug

--- flash/settings.py
+++ flash/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uek9_@c35c9ahasur3f7h3v3^w0pwmrvr&^6zt=9&qp6*qu1))'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['chikos96-flash-ecommerce.herokuapp.com', 'localhost']
 
